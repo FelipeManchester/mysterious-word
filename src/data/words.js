bd49e4366c2003960ecce61f4240a85d6c169f19
Felipe Manchester <f.p2809@gmail.com>
2022-10-31 18:50:12 -0300

    Updates

--- src/data/words.js
+++
@@ -1,8 +0,0 @@
-export const wordsList = {
-  carro: ['Ford', 'Chevrolet', 'Fiat', 'Ferrari', 'Jeep'],
-  fruta: ['Banana', 'Tangerina', 'Uva', 'Kiwi', 'Laranja'],
-  corpo: ['Orelha', 'Perna', 'Joelho', 'Queixo', 'Olhos'],
-  computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete'],
-  programação: ['Python', 'Framework', 'JavaScript', 'React'],
-  alimento: ['Arroz', 'Peixe', 'Carne', 'Leite', 'Ovo'],
-};
